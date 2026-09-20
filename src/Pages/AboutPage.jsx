3457270ded13ada8015d9a6403168f5224a2c440
Mustapha Abdulai <mazbandago@gmail.com>
2026-09-20 13:20:29 +0000

    added the navbar to home and about page

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function AboutPage() {
   return (
-   <h1>This is the AboutPage</h1>
+    <section>
+        <NavBar/>
+        <h1>This is the AboutPage</h1>
+    </section>
   )
 }
 
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function HomePage() {
   return (
-    <h1>This is the HomePage</h1>
+    <section>
+        <NavBar/>
+        <h1>This is the HomePage</h1>
+    </section>
   )
 }
 
